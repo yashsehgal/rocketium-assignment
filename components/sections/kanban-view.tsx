95ae9eb968036f8499bfbf30b9d083888c32a991
Yash Sehgal <yashsehgal.work@gmail.com>
2024-02-16 16:48:25 +0530

    Localstorage update on drag/drop

--- components/sections/kanban-view.tsx
+++ components/sections/kanban-view.tsx
@@ -1,15 +1,31 @@
-'use client';
-import { DragDropContext, DropResult } from 'react-beautiful-dnd';
+import { DragDropContext, DragStart, DragUpdate, DropResult } from 'react-beautiful-dnd';
 import { ResponsiveControl } from '../layouts/responsive-control';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { KanbanBoardContext } from '@/contexts/kanban-board-context';
 import { KanbanList } from '../ui/kanban-list';
 
 export default function KanbanView() {
-  const { filteredKanbanData, setFilteredKanbanData } =
-    useContext(KanbanBoardContext);
+  const { filteredKanbanData, setFilteredKanbanData } = useContext(KanbanBoardContext);
+  const [draggedItem, setDraggedItem] = useState<KanbanCardType | null>(null);
+  const [draggedOverIndex, setDraggedOverIndex] = useState<number | null>(null);
+
+  const onDragStart = (initial: DragStart) => {
+    const { source } = initial;
+    const sourceList = filteredKanbanData.find((list) => list.listName === source.droppableId);
+    const draggedTask = sourceList?.listItems[source.index];
+    setDraggedItem(draggedTask || null);
+  };
+
+  const onDragUpdate = (update: DragUpdate) => {
+    const { destination } = update;
+    setDraggedOverIndex(destination?.index || null);
+  };
 
   const onDragEnd = (result: DropResult) => {
+    // Reset state
+    setDraggedItem(null);
+    setDraggedOverIndex(null);
+
     // Dropped outside the list
     if (!result.destination) {
       return;
@@ -19,19 +35,21 @@ export default function KanbanView() {
 
     // Update the data after dragging and dropping
     const updatedKanbanData = [...filteredKanbanData];
-    const sourceList = updatedKanbanData.find(
-      (list) => list.listName === source.droppableId,
-    );
-    const destinationList = updatedKanbanData.find(
-      (list) => list.listName === destination.droppableId,
-    );
-    const [draggedItem] = sourceList!.listItems.splice(source.index, 1);
-    destinationList!.listItems.splice(destination.index, 0, draggedItem);
+    const sourceList = updatedKanbanData.find((list) => list.listName === source.droppableId);
+    const destinationList = updatedKanbanData.find((list) => list.listName === destination.droppableId);
+    const [draggedTask] = sourceList!.listItems.splice(source.index, 1);
+    destinationList!.listItems.splice(destination.index, 0, draggedTask);
+
+    // Update the local storage with the latest Kanban data
+    localStorage.setItem('kanban', JSON.stringify(updatedKanbanData));
+
+    // Update the state to trigger a re-render
+    setFilteredKanbanData(updatedKanbanData);
   };
 
   return (
     <div className="kanban-view py-12">
-      <DragDropContext onDragEnd={onDragEnd}>
+      <DragDropContext onDragStart={onDragStart} onDragUpdate={onDragUpdate} onDragEnd={onDragEnd}>
         <ResponsiveControl className="flex flex-row items-start justify-start gap-3 max-xl:overflow-x-scroll">
           {filteredKanbanData.map((list: KanbanListType, index: number) => {
             return <KanbanList key={index} index={index} {...list} />;
--- components/ui/kanban-list.tsx
+++ components/ui/kanban-list.tsx
@@ -1,12 +1,11 @@
-'use client';
 import { cn } from '@/helpers/utils';
-import { forwardRef } from 'react';
+import { forwardRef, ReactNode } from 'react';
 import { KanbanCard } from './kanban-card';
+import { Draggable, Droppable } from 'react-beautiful-dnd';
 
-export interface KanbanListProps
-  extends React.HTMLAttributes<HTMLDivElement>,
-    KanbanListType {
+export interface KanbanListProps extends React.HTMLAttributes<HTMLDivElement>, KanbanListType {
   index: number;
+  children?: ReactNode;
 }
 
 export const KanbanList = forwardRef<HTMLDivElement, KanbanListProps>(
@@ -14,25 +13,38 @@ export const KanbanList = forwardRef<HTMLDivElement, KanbanListProps>(
     return (
       <div>
         <div className="kanban-list-details-wrapper px-2 mb-2">
-          <p className="leading-snug font-medium tracking-tight text-gray-500 text-sm">
-            {listName}
-          </p>
+          <p className="leading-snug font-medium tracking-tight text-gray-500 text-sm">{listName}</p>
         </div>
-        <div
-          ref={ref}
-          className={cn(
-            'kanban-list p-2 border-b-4 border-transparent rounded-xl bg-gray-100/80 min-w-[280px] w-fit max-w-[300px] h-[600px] overflow-y-scroll',
-            className,
+        <Droppable droppableId={listName}>
+          {(provided, snapshot) => (
+            <div
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              className={cn(
+                'kanban-list p-2 border-b-4 border-transparent rounded-xl bg-gray-100/80 min-w-[280px] w-fit max-w-[300px] h-[600px] overflow-y-scroll',
+                className,
+              )}
+              {...args}>
+              <div className="grid grid-cols-1 gap-2">
+                {listItems.map((card: KanbanCardType, index: number) => {
+                  return <>
+                    <Draggable draggableId={`${card.taskTitle}-${card.ticketID}`} index={index}>
+                      {(provided) => (<KanbanCard
+                        key={index}
+                        {...card}
+                        index={index}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        ref={provided.innerRef}
+                      />)}
+                    </Draggable>
+                  </>
+                })}
+              </div >
+              {provided.placeholder}
+            </div>
           )}
-          {...args}>
-          <div className="grid grid-cols-1 gap-2">
-            {listItems.map((card: KanbanCardType, index: number) => {
-              return (
-                <KanbanCard key={index} index={index} draggable {...card} />
-              );
-            })}
-          </div>
-        </div>
+        </Droppable>
       </div>
     );
   },
